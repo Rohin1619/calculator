455ed391bbf4cb245d8edd8160a6ad7328d22a73
Rohin Bharatee <rohinbharatee@gmail.com>
2023-09-05 13:33:40 +0545

    Initial commit

---
+++ src/Page/Calculator.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../Components/Button'
+import './page.css'
+
+
+const Calculator = () => {
+    return (
+        <>
+            <h2>CALCULATOR</h2>
+            <label className='labels'>123</label>
+            <Button/>
+        </>
+    )
+}
+
+export default Calculator
